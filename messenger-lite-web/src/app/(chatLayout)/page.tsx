656f9fd922882f37ab3c1ab9273c78edafaccd60
Rjk-Jami <rjkhan.1221@gmail.com>
2025-08-03 00:53:09 +0600

    Initializing chat layout

---
+++ messenger-lite-web/src/app/(chatLayout)/page.tsx
@@ -0,0 +1,12 @@
+import ChatLayout from '@/components/ChatLayout/ChatLayout'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <ChatLayout />
+        </div>
+    )
+}
+
+export default page
---
+++ messenger-lite-web/src/components/ChatLayout/ChatLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatLayout = () => {
+    return (
+        <div className='`h-screen flex flex-col bg-gray-50 dark:bg-gray-900'>
+
+        </div>
+    )
+}
+
+export default ChatLayout
