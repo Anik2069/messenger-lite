9b772b44ce490ec807a131665936de603e1a9539
Jami Khan <rjkhan.1221@gmail.com>
2025-08-16 00:46:32 +0600

    sign in route initialize

---
+++ messenger-lite-api/src/libs/sendResponse.ts
@@ -0,0 +1,24 @@
+import { Response } from "express";
+
+interface SendResponseOptions<T = any> {
+  res: Response;
+  statusCode: number;
+  message: string;
+  data?: T | null;
+}
+
+const sendResponse = <T = any>({
+  res,
+  statusCode,
+  message,
+  data = null,
+}: SendResponseOptions<T>): void => {
+  res.status(statusCode).json({
+    statusCode,
+    message,
+    results: data,
+    timestamp: new Date().toISOString(),
+  });
+};
+
+export default sendResponse;
--- messenger-lite-api/src/routes/v1/auth/routes/user_auth.routes.ts
+++ messenger-lite-api/src/routes/v1/auth/routes/user_auth.routes.ts
@@ -1,8 +1,10 @@
+import { userSignin } from "../../../../controllers/auth/userSignin.controller";
 import { userSignup } from "../../../../controllers/auth/userSignup.controller";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/sign-up", userSignup);
+router.post("/sign-in", userSignin);
 
 export default router;
