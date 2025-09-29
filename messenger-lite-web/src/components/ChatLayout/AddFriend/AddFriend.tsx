e9e612dc47107210a90837a44f16e5b04d51c1df
Jami Khan <rjkhan.1221@gmail.com>
2025-09-30 02:06:17 +0600

    Implement Tabs switch at Add Friend drawer for show Suggestions, Requested User and pending Request

--- messenger-lite-web/src/components/ChatLayout/AddFriend/AddFriend.tsx
+++ messenger-lite-web/src/components/ChatLayout/AddFriend/AddFriend.tsx
@@ -2,6 +2,7 @@
 import { DummyAvatar } from "@/assets/image";
 import ReusableSearchInput from "@/components/reusable/ReusableSearchInput";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useFriendsStore } from "@/store/useFriendsStrore";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
@@ -19,56 +20,120 @@ const AddFriend = () => {
     fetchAllFriends(searchText);
   }, [searchText, fetchAllFriends]);
   return (
-    <div className="space-y-2  ">
-      <div className="p-4">
-        <ReusableSearchInput
-          placeholder="Search Name"
-          onDebouncedChange={setSearchText}
-        />
-      </div>
-      <div className="">
-        {friends?.map((userInfo) => {
-          return (
-            <div
-              key={userInfo?.id}
-              className={`flex items-center px-4 py-3 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors `}
-            >
-              <div className="relative mr-3">
-                <Image
-                  src={userInfo?.avatar || DummyAvatar}
-                  alt={userInfo?.username}
-                  width={40}
-                  height={40}
-                  className="w-10 h-10 rounded-full object-cover cursor-pointer "
-                />
-              </div>
+    <div className="">
+      <Tabs defaultValue="suggestion" className="p-4">
+        <TabsList className="w-full ">
+          <TabsTrigger value="suggestion">Suggestion</TabsTrigger>
+          <TabsTrigger value="request">Request</TabsTrigger>
+          <TabsTrigger value="pending">Pending</TabsTrigger>
+        </TabsList>
+        <div className="mt-2">
+          <ReusableSearchInput
+            placeholder="Search Name"
+            onDebouncedChange={setSearchText}
+          />
+        </div>
+        <TabsContent value="suggestion">
+          <div className="">
+            {friends?.map((userInfo) => {
+              return (
+                <div
+                  key={userInfo?.id}
+                  className={`flex items-center px-4 py-3 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors `}
+                >
+                  <div className="relative mr-3">
+                    <Image
+                      src={userInfo?.avatar || DummyAvatar}
+                      alt={userInfo?.username}
+                      width={40}
+                      height={40}
+                      className="w-10 h-10 rounded-full object-cover cursor-pointer "
+                    />
+                  </div>
 
-              <div className="flex-1 min-w-0 flex justify-between">
-                <div className="">
-                  <h3 className="font-medium text-gray-900 dark:text-white truncate cursor-pointer w-fit">
-                    {userInfo?.username}
-                  </h3>
-                  <p
-                    className={`text-xs truncate text-gray-500 dark:text-gray-400 
+                  <div className="flex-1 min-w-0 flex justify-between">
+                    <div className="">
+                      <h3 className="font-medium text-gray-900 dark:text-white truncate cursor-pointer w-fit">
+                        {userInfo?.username}
+                      </h3>
+                      <p
+                        className={`text-xs truncate text-gray-500 dark:text-gray-400 
                   `}
-                  >
-                    10 mutual friends
-                  </p>
+                      >
+                        10 mutual friends
+                      </p>
+                    </div>
+                    <div className="my-auto">
+                      <Button
+                        className="cursor-pointer"
+                        size={"sm"}
+                        variant={"default"}
+                      >
+                        Add Friend
+                      </Button>
+                    </div>
+                  </div>
                 </div>
-                <div className="my-auto">
-                  <Button
-                    className="cursor-pointer"
-                    size={"sm"}
-                    variant={"default"}
-                  >
-                    Add Friend
-                  </Button>
+              );
+            })}
+          </div>
+        </TabsContent>
+        <TabsContent value="request">
+          <div className="">
+            {friends?.map((userInfo) => {
+              return (
+                <div
+                  key={userInfo?.id}
+                  className={`flex items-start px-4 py-3 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors `}
+                >
+                  <div className="relative mr-3">
+                    <Image
+                      src={userInfo?.avatar || DummyAvatar}
+                      alt={userInfo?.username}
+                      width={40}
+                      height={40}
+                      className="w-10 h-10 rounded-full object-cover cursor-pointer mt-1"
+                    />
+                  </div>
+
+                  <div className="flex-1 min-w-0 flex flex-col gap-1">
+                    <div className="">
+                      <h3 className="font-medium text-gray-900 dark:text-white truncate cursor-pointer w-fit">
+                        {userInfo?.username}
+                      </h3>
+                      <p
+                        className={`text-xs truncate text-gray-500 dark:text-gray-400 
+                  `}
+                      >
+                        10 mutual friends
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <Button
+                        className="cursor-pointer"
+                        size={"sm"}
+                        variant={"default"}
+                      >
+                        Confirm
+                      </Button>
+                      <Button
+                        className="cursor-pointer"
+                        size={"sm"}
+                        variant={"outline"}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </TabsContent>
+        <TabsContent value="pending">
+          <div className="">Pending</div>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
