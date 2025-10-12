7443a373bd0f34b8dfb606dcc5ee2287232ad9f8
Jami Khan <rjkhan.1221@gmail.com>
2025-10-12 14:16:35 +0600

    format prisma

--- messenger-lite-web/src/components/ChatLayout/UserSettings/@privacy/TwoFactorAuth.tsx
+++ messenger-lite-web/src/components/ChatLayout/UserSettings/@privacy/TwoFactorAuth.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const TwoFactorAuth = () => {
   return (
-    <div>
+    <div className="border rounded-xl p-4 border-gray-200 dark:border-gray-700 pb-4">
       <h2 className="text-xl font-semibold mb-4">2FA Authentication</h2>
       <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">
         Secure your account by enabling two-factor authentication using Google
