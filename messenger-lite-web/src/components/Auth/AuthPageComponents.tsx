3f59c9776f86dfe2cc89162093f3965c939758a9
Jami Khan <rjkhan.1221@gmail.com>
2025-10-13 22:53:46 +0600

    add loading ui at sign in

--- messenger-lite-web/src/components/Auth/AuthPageComponents.tsx
+++ messenger-lite-web/src/components/Auth/AuthPageComponents.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { AuthForm } from "./AuthForm";
 import { useAuth } from "@/context/useAuth";
 import { SubmitOtpForm } from "./SubmitOtpForm";
+import Loading from "@/app/loading";
 
 const AuthPageComponents = () => {
-  const { is2FAEnabled } = useAuth();
-  return <div>{is2FAEnabled ? <SubmitOtpForm /> : <AuthForm />}</div>;
+  const { is2FAEnabled, loading } = useAuth();
+
+  return (
+    <div>
+      {loading ? <Loading /> : is2FAEnabled ? <SubmitOtpForm /> : <AuthForm />}
+    </div>
+  );
 };
 
 export default AuthPageComponents;
