0ef6d0d56bba5e7baf8429ba19ff32ada1fd5bdf
abnafis17 <bakhtiyarnafis17@gmail.com>
2025-11-05 22:17:26 +0600

    Add additional image domain to next.config.ts

--- messenger-lite-web/next.config.ts
+++ messenger-lite-web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["192.168.31.152"],
+    domains: ["192.168.31.152", "192.168.1.102"],
   },
 };
 
