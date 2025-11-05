ba1ad7cfbb4f1201490f681984eadce269474b7a
Jami Khan <rjkhan.1221@gmail.com>
2025-11-06 02:00:31 +0600

    updated

--- messenger-lite-web/next.config.ts
+++ messenger-lite-web/next.config.ts
@@ -2,7 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["192.168.31.152", "10.81.100.28"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "192.168.31.152",
+        port: "7000", // optional
+        pathname: "/**", // allow all paths
+      },
+      {
+        protocol: "http",
+        hostname: "10.81.100.28",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "messenger-lite-ijbp.onrender.com",
+        pathname: "/**", // allow all paths
+      },
+    ],
   },
 };
 
